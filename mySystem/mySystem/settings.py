51a5102fd18949e3402b3e225b77fd934fae945c
ajitrakate <62492701+ajitrakate@users.noreply.github.com>
2022-03-24 00:39:56 +0530

    Update settings.py

--- mySystem/mySystem/settings.py
+++ mySystem/mySystem/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zi@bojq8z4xz-u=r+#jqmwuk24cun^%=4g4)%l%ch*&6-33o)-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Default primary key field type
+#django-insecure-zi@bojq8z4xz-u=r+#jqmwuk24cun^%=4g4)%l%ch*&6-33o)-
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
